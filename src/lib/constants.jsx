6795d485e3fbb3b4f9d068ceb036ae69660e322c
yachirenn <rendysulistyawan11@gmail.com>
2025-10-24 22:11:30 +0700

    mudif

--- src/lib/constants.jsx
+++
@@ -1,8 +0,0 @@
-export const navItems = [
-  { name: "Beranda", href: "#main" },
-  { name: "Tentang", href: "#about" },
-  { name: "Kenapa Kami", href: "#kategori" },
-  { name: "Kegiatan", href: "#aktivitas" },
-  { name: "Testimoni", href: "#testimoni" },
-  { name: "Kontak", href: "#footer" },
-];
