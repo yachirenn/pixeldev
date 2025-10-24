7a3b211b7dda66a56e39dc5483437aa943d49876
yachirenn <rendysulistyawan11@gmail.com>
2025-10-24 22:15:45 +0700

    Re-add frontend as regular folder

---
+++ frontend/src/lib/constants.jsx
@@ -0,0 +1,8 @@
+export const navItems = [
+  { name: "Beranda", href: "#main" },
+  { name: "Tentang", href: "#about" },
+  { name: "Kenapa Kami", href: "#kategori" },
+  { name: "Kegiatan", href: "#aktivitas" },
+  { name: "Testimoni", href: "#testimoni" },
+  { name: "Kontak", href: "#footer" },
+];
