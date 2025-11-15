99fc99972b7982322e80dd6949611e985a863762
yachirenn <rendysulistyawan11@gmail.com>
2025-11-15 15:52:25 +0700

    update environment

--- frontend/src/app/api/sendEmail.jsx
+++ frontend/src/app/api/sendEmail.jsx
@@ -1,17 +1,14 @@
-import nodemailer from "nodemailer";
-
 export async function POST(req) {
   const { name, email, message } = await req.json();
 
-  // Kirim ke backend kamu di port 5000
-  const response = await fetch("http://localhost:5000/api/email", {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/send`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ name, email, message }),
   });
-  
+
   const result = await response.json();
 
   return new Response(JSON.stringify(result), {
