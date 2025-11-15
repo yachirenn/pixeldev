ea048d7ed63e6e9feaebaa0506c28ba85c9df1d3
yachirenn <rendysulistyawan11@gmail.com>
2025-11-15 16:41:02 +0700

    update environment

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://pixeldev-eosin.vercel.app/'
+}));
 app.use(express.json());
 
 // Routing untuk form email
--- backend/routes/email.js
+++ backend/routes/email.js
@@ -4,7 +4,8 @@ import { sendEmail } from '../controller/emailController.js';
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    console.log('Request masuk ke /send:', req.body);
+  console.log('Request masuk:', req.body);
+  res.json({ success: true, message: 'Email berhasil diterima' });
 });
 
 export default router;
