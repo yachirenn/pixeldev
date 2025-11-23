62810968c2e712562515c27a8086275e32032fa0
yachirenn <rendysulistyawan11@gmail.com>
2025-11-23 17:37:45 +0700

    update pages

--- fullstack/src/app/layout.tsx
+++ fullstack/src/app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/logo-tkip.png" type="image/png" />
+      </head>
       <body
         className={`${inter.variable} ${poppins.variable} antialiased`}
       >
