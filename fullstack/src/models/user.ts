f3fbc2b5e6fffd7e49af0fa12622074555260344
yachirenn <rendysulistyawan11@gmail.com>
2026-05-07 11:07:05 +0700

    feat : build a connection database for admin dashboard with mongoDB

---
+++ fullstack/src/models/user.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    role: { type: String, default: "client" },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.models.User ||
+  mongoose.model("User", UserSchema)
